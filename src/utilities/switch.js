6432b5db5e347e7324f9bcbf81a75e893c7927c8
Chris Sullivan <chrissullivan.dev@gmail.com>
2019-05-02 19:01:22 -0700

    Adding tests

--- src/utilities/switch.js
+++ src/utilities/switch.js
@@ -1,13 +1,6 @@
 
 module.exports = {
   id: 'fn::switch',
-  resolve: ({ key, cases }) => {
-    for (const match of Object.keys(cases)) {
-      if (key === match) {
-        return cases[match];
-      }
-    }
-    return cases['*'];
-  },
+  resolve: ({ key, cases }) => cases[key] || cases['*'],
 }
 
